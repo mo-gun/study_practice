6933283ccd7412e387d48e3aafd37bc4172a0625
geonmo lee <leegunmo1103@gmail.com>
2026-03-24 22:01:12 +0900

    feat: HelloService 구현

--- src/main/java/com/ll/study/HelloService.java
+++ src/main/java/com/ll/study/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String hi() {
-        return "Hello, World!";
+        return "Hello, World! new try!";
     }
 }
